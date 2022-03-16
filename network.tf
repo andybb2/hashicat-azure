73d9ee3bc0ad2c890443e2a4f064f678d96c9098
Andrew B <andybrand@gmail.com>
2022-03-16 03:28:14 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -4,3 +4,8 @@ module "network" {
   # insert required variables here
   resource_group_name = "test_resource_group_name"
 }
+
+resource "azurerm_resource_group" "network" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+}
