23c96ca2fd41f5df0c21230583105712455783c9
Andrew B <andybrand@gmail.com>
2022-03-16 03:35:25 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,10 +2,5 @@ module "network" {
   source  = "app.terraform.io/lab_company/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "test_resource_group_name"
-}
-
-resource "azurerm_resource_group" "network" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
+  resource_group_name = "${var.prefix}-workshop"
 }
